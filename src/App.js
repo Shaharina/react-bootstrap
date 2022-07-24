041422152b14263bd40d4d937b083829fbfb16f8
Shaharina Mita <S.mita90232@gmail.com>
2022-07-25 00:33:33 +0600

    style added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <h1>Using bootstrap</h1>
+            <Button variant='danger'>My button</Button>
+        </div>
+    );
 }
 
 export default App;
